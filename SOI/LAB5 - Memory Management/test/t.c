84f6d8e78fa73efb717c3d80c4cb69d23fe77d91
Paul Walczak <walczakp007@gmail.com>
2017-01-03 11:37:24 -0800

    Test scripts added

---
+++ SOI/LAB5 - Memory Management/test/t.c
@@ -0,0 +1,28 @@
+/* t.c - polecenie t wyswietla liczbe i rozmiary blokow wolnych */
+#include <stdio.h>
+#include <unistd.h>
+#include <lib.h>
+                                                                                
+PUBLIC int hole_map( void *buffer, size_t nbytes)
+{
+	/* ... _syscall(..HOLE_MAP..) ... */
+}
+                                                                                
+int
+main( void )
+{
+        unsigned int    b[1024];
+        unsigned int    *p, a, l;
+        int res;
+	res = hole_map( b, sizeof( b ) );
+        printf( "[%d]\t", res );
+        p = b;
+        while( *p )
+        {
+                l = *p++;
+                a = *p++; /* tu niewykorzystywane */
+                printf( "%d\t", l );
+        }
+        printf( "\n" );
+        return 0;
+}
