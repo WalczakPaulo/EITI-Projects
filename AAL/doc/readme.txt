7632ba0610ffb6f306af3c453d562ab4215a06c1
Paul <walczakp007@gmail.com>
2017-01-06 18:59:42 +0100

    new test added

--- AAL/doc/readme.txt
+++
@@ -1,4 +0,0 @@
-Dokumentacja projektu prowadzona w jêzyku polskim
-
-[ENG]
-Documentation of project, maintained only in polish language!
---
+++ AAL/test/OptimalAlgorithmTests.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Paul on 2017-01-06.
+//
+
+#include "gtest/gtest.h"
+#include "OptimalAlgorithm.h"
+
+class OptimalAlgorithmTest : public ::testing::Test {
+public:
+    OptimalAlgorithm optimal;
+};
+
